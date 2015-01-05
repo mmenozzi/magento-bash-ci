d9726cdbf9d21a58bcf75c0b4d2520fa5f3c5da2
Manuele Menozzi <manuele.menozzi@gmail.com>
2015-01-05 18:49:43 +0100

    Adds composer-command-integrator support

--- ci-install.sh
+++ ci-install.sh
@@ -3,6 +3,8 @@
 cd ${BASE_DIR}
 
 # Gathering dependencies path
+PHP_PATH=`which php`
+
 N98_PATH=`which n98-magerun.phar`
 if [ -z "$N98_PATH" ]; then
     wget https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar
@@ -22,16 +24,29 @@ if [ -z "$MODMAN_PATH" ]; then
     MODMAN_PATH="${BASE_DIR}/modman.sh"
 fi
 
+COMPOSER_COMMAND_INTEGRATOR_PATH="";
+if [ -f ${BASE_DIR}/vendor/bin/composerCommandIntegrator.php ]; then
+    COMPOSER_COMMAND_INTEGRATOR_PATH="${BASE_DIR}/vendor/bin/composerCommandIntegrator.php";
+fi
+
 # Installing Magento
 mysql -uroot --password="${db_pass}" -e "DROP DATABASE IF EXISTS \`${db_name}\`"
 rm -f "${BASE_DIR}/${magento_dir}/app/etc/local.xml"
 if [ -f "${BASE_DIR}/${magento_dir}/app/Mage.php" ]; then
     # Installing composer dependencies
     ${COMPOSER_PATH} install
+    # Sometimes it's needed to run composer-command-integrator to successfully deploy extensions
+    if [ ! -z "$COMPOSER_COMMAND_INTEGRATOR_PATH" ]; then
+        php ${COMPOSER_COMMAND_INTEGRATOR_PATH} magento-module-deploy
+    fi
     php ${N98_PATH} install --noDownload --installationFolder=${magento_dir} --dbHost=${db_host} --dbUser=${db_user} --dbPass=${db_pass} --dbName=${db_name} --installSampleData=${install_sample_data} --useDefaultConfigParams=yes --magentoVersionByName=${MAGENTO_VERSION} --baseUrl=${base_url}
 else
     php ${N98_PATH} install --installationFolder=${magento_dir} --dbHost=${db_host} --dbUser=${db_user} --dbPass=${db_pass} --dbName=${db_name} --installSampleData=${install_sample_data} --useDefaultConfigParams=yes --magentoVersionByName=${MAGENTO_VERSION} --baseUrl=${base_url}
     ${COMPOSER_PATH} install
+    # Sometimes it's needed to run composer-command-integrator to successfully deploy extensions
+    if [ ! -z "$COMPOSER_COMMAND_INTEGRATOR_PATH" ]; then
+        php ${COMPOSER_COMMAND_INTEGRATOR_PATH} magento-module-deploy
+    fi
 fi
 cd ${magento_dir} && php ${N98_PATH} cache:clean && cd ${BASE_DIR}
 cd ${magento_dir} && php ${N98_PATH} cache:disable && cd ${BASE_DIR}
